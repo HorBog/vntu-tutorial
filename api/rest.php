87aa2bc0763a0ed80fc6598874806693900bdd70
Alex Slubsky <aslubsky@gmail.com>
2016-11-14 21:49:51 +0200

    add JWT Auth

--- api/rest.php
+++ api/rest.php
@@ -1,12 +1,15 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Firebase\JWT\JWT;
 
 require 'vendor/autoload.php';
 require 'db.php';
 
 $app = new \Slim\App;
 DB::init('mysql:dbname=information_schema;host=127.0.0.1;port=3306', 'root', '');
+
+define('AUTH_SECRET', "supersecretkeyyoushouldnotcommittogithub");
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
@@ -18,6 +21,66 @@ $app->add(function ($req, $res, $next) {
         ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
+/* auth */
+//$app->add(new \Slim\Middleware\JwtAuthentication([
+//    "secret" => "supersecretkeyyoushouldnotcommittogithub"
+//]));
+
+
+$app->add(new \Slim\Middleware\JwtAuthentication([
+//    "logger" => $logger,
+    "secret" => AUTH_SECRET,
+    "rules" => [
+        new \Slim\Middleware\JwtAuthentication\RequestPathRule([
+//            "path" => "/api",
+            "passthrough" => ["/token"]
+        ]),
+        new \Slim\Middleware\JwtAuthentication\RequestMethodRule([
+            "passthrough" => ["OPTIONS"]
+        ])
+    ]
+]));
+
+//$app->add(new \Slim\Middleware\HttpBasicAuthentication([
+//    "path" => "/api/token",
+//    "users" => [
+//        "user" => "password"
+//    ]s
+//]));
+
+$app->post("/token", function (Request $request, Response $response) {
+    /* Here generate and return JWT to the client. */
+//    exit('O_o');
+    $data = $request->getParsedBody();
+
+    $expireTime = 60 * 60;//in seconds
+
+    $login = isset($data['login']) ? $data['login'] : '';
+    $password = isset($data['password']) ? $data['password'] : '';
+    if ($login == '1' && $password == '1') {
+        $token = JWT::encode([
+            "iss" => 1,
+            'exp' => time() + $expireTime,
+            'nbf' => time()
+        ],
+            AUTH_SECRET
+        );
+        $response->getBody()->write(json_encode([
+            'id' => 1,
+            'login' => $login,
+            'token' => $token
+        ]));
+    } else {
+        $response = $response->withStatus(400);
+        $response->getBody()->write(json_encode([
+            'error' => 'Invalid login'
+        ]));
+    }
+
+    return $response;
+
 });
 
 $app->get('/posts', function (Request $request, Response $response) {
--- app/main.js
+++ app/main.js
@@ -4,6 +4,36 @@ mainApp.factory('PostModel', ['$resource', function ($resource) {
     return $resource('http://localhost:8088/api/rest.php/posts/:id', {'id': '@id'}, {});
 }]);
 
+mainApp.factory('AuthModel', ['$resource', function ($resource) {
+    return $resource('http://localhost:8088/api/rest.php/token/:id', {'id': '@id'}, {
+        'login': {'method': 'POST'}
+    });
+}]);
+
+mainApp.factory('jwtInterceptor', ['$rootScope', '$q', function ($rootScope, $q) {
+    return {
+        request: function (config) {
+            var token = window.localStorage.getItem('auth_token');
+            config.headers = config.headers || {};
+            if (token != 'undefined' && angular.isDefined(token)) {
+                config.headers.Authorization = 'Bearer ' + token;
+            }
+            return config;
+        },
+        response: function (response) {
+            if (response.status === 401) {
+                // handle the case where the user is not authenticated
+            }
+            return response || $q.when(response);
+        }
+    };
+}]);
+
+mainApp.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('jwtInterceptor');
+}]);
+
+
 
 mainApp.controller('MainController', ['$scope', 'PostModel', function ($scope, PostModel) {
     $scope.answer = ' - ';
@@ -13,9 +43,25 @@ mainApp.controller('MainController', ['$scope', 'PostModel', function ($scope, P
     };
 
     $scope.getPosts = function (answer) {
-        PostModel.get(function(res) {
+        PostModel.get(function (res) {
             console.log('res', res.data);
             $scope.posts = res.data;
+        });
+    };
+}]);
+
+
+mainApp.controller('LoginController', ['$scope', 'AuthModel', function ($scope, AuthModel) {
+    $scope.user = {};
+
+    $scope.login = function () {
+        console.log('user', $scope.user);
+
+        AuthModel.login($scope.user, function (res) {
+            console.log('res', res.toJSON());
+            window.localStorage.setItem('auth_token', res.toJSON().token);
+        }, function (err) {
+            console.log('err', err.data);
         });
     };
 }]);
